1f6ed3328a75fd4f7b90c5edb34fa7bd1bc0642b
Willian Braga Cassiano <llchivell@gmail.com>
2025-11-07 22:07:02 -0400

    remocao configuracao desnecessaria

--- src/config/chatwoot.js
+++
@@ -1,3 +0,0 @@
-export const CHATWOOT_WEBSITE_TOKEN = "COLE_SEU_TOKEN_AQUI";
-export const CHATWOOT_BASE_URL = "https://chat.controleweb.dev.br";
-// Se você tiver self-hosted, troque pelo domínio do seu Chatwoot.
